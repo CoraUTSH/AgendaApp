d98354fb1167d9a2ef96b062b03c6b4b893a4cc5
cora <alberto.corona@utsh.edu.mx>
2023-07-19 20:45:06 -0600

    ConexionBD-2

--- app/src/main/java/com/example/agendaapp/MainActivity.kt
+++ app/src/main/java/com/example/agendaapp/MainActivity.kt
@@ -33,7 +33,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
+
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.compose.NavHost
